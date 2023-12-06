d9cb81b88bd90cd9da58a3e5d008822b2f852f77
Carla Antonini <carla.antonini@hotmail.com>
2023-12-06 01:25:25 -0300

    Agregado de set System Info Version de Selenium utilizada

--- src/main/java/Reports/ExtentFactory.java
+++ src/main/java/Reports/ExtentFactory.java
@@ -5,6 +5,7 @@ public class ExtentFactory {
     public static ExtentReports getInstance() {
         ExtentReports extent = new ExtentReports();
         extent.setSystemInfo("Environment", "PROD");
+        extent.setSystemInfo("Selenium Version", "4.1.4");
         extent.setSystemInfo("OS", "Windows");
         extent.setSystemInfo("Browser", "Chrome");
         return extent;
