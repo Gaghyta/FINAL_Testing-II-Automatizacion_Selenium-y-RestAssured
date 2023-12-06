1267920eb32b57c37d6bc6e1c6815d236a55644b
Carla Antonini <carla.antonini@hotmail.com>
2023-12-05 21:21:27 -0300

    Cambios en estructura + BASE PAGE con métodos: setUp, webElement y elementFind

---
+++ src/main/java/Reports/ExtentFactory.java
@@ -0,0 +1,12 @@
+package Reports;
+import com.aventstack.extentreports.ExtentReports;
+
+public class ExtentFactory {
+    public static ExtentReports getInstance() {
+        ExtentReports extent = new ExtentReports();
+        extent.setSystemInfo("Environment", "PROD");
+        extent.setSystemInfo("OS", "Windows");
+        extent.setSystemInfo("Browser", "Chrome");
+        return extent;
+    }
+}
